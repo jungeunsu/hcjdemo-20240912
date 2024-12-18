8ddbab83c2214f88a9338dc505da2cb45dec5f55
Eunsu <twin6086@naver.com>
2024-12-18 21:12:26 +0900

    2st

--- main.js
+++ main.js
@@ -23,9 +23,12 @@ function showcss() {
   document.getElementById('desc').innerHTML =
     '<b>CSS3</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
 }
-
 function showjs() {
   document.getElementById('fig').src = 'img/html5.jpg'
   document.getElementById('desc').innerHTML =
     '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
+}
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
 }
